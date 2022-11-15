73153b8a9b30457a566cce83bedeb2ec6a846658
DiegoRodriguez1203 <diegorodriguez1203@gmail.com>
2022-11-15 10:19:05 -0500

    important const that will be used along the code

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//  IMPORTANT CONST THAT WILL BE USED ALONG THE CODE
+
+
+// The const infite while loope to make the game run
+const GAME_CLOCK = 1000;
+// THE WIDE OF EACH BLOCK IN THE GAME
+const BLOCK_SIDE_LENGTH = 30;
+
+// NUMBERS OF ROWS AND COLUMNS THAT WILL BE USED IN THE GAME
+const ROWS = 20;
+const COLS = 10;
+// Score given to the gamer for each complete row realized
+const SCORE_WORTH = 10;
